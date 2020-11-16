90529e29c263dd63906bfe9d3df59814d1833ba9
Erin Scully <erin3scully@icloud.com>
2020-11-15 20:38:01 -0700

    Initial Commit

---
+++ Cloud~WeatherTests/Cloud_WeatherTests.swift
@@ -0,0 +1,33 @@
+//
+//  Cloud_WeatherTests.swift
+//  Cloud~WeatherTests
+//
+//  Created by Erin Scully on 11/15/20.
+//
+
+import XCTest
+@testable import Cloud_Weather
+
+class Cloud_WeatherTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
